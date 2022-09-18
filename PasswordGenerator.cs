57daeaa4f4e7663edac73a54fc36e1134eb32e2a
Cristian Farcas <christian.farcas@gmail.com>
2022-09-18 20:22:08 +0300

    Initial commit

---
+++ PasswordGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCrafters_OTP
+{
+    public class PasswordGenerator
+    {
+        public int Generate(int userId, DateTime dateTime)
+        {
+            var seed = userId + (int)dateTime.Ticks;
+            var rand = new Random(seed);
+            return rand.Next(10000,99999);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using CodeCrafters_OTP;
+using System;
+
+public class Program
+{
+    private readonly PasswordGenerator passwordGenerator;
+    public Program(PasswordGenerator passwordGenerator)
+    {
+        this.passwordGenerator = passwordGenerator;
+    }
+    Random random = new Random();
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Please enter your Id:");
+        var userId2 = Console.ReadLine();
+        DateTime datetime = DateTime.Now;
+        int Id;
+        var success = int.TryParse(userId2, out Id);
+        if (!success)
+        {
+            Console.WriteLine("This is not a valid userId");
+            return;
+        }
+        var generator = new PasswordGenerator();
+
+        Timer timer = new System.Threading.Timer(o => { generator.Generate(Id, datetime); }, null,0,3000);
+       
+    }
+   
+}
+
+
